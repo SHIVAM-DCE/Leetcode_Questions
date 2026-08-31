b2bfa76d0892502eb5c769470b847d44cf726e9e
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-08-31 23:11:12 +0530

    Time: 0 ms (100%), Space: 8.4 MB (83.5%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,24 +1,21 @@
-class Solution{
+class Solution {
 public:
-    int reverse(int n){
-    int rem=0;
-    int ans=0;
-    bool isnegetive=false;
-    if(n<=INT_MIN){
-        return 0;
-    }
-    if(n<0){
-        isnegetive=true;
-        n=-n;
-    }
-    while(n>0){
-         if(ans>INT_MAX/10){
+    int reverse(int x) {
+        bool isNegetive=false;
+        long long int num=x;
+        if(num<0){
+            isNegetive=true;
+            num = -num;
+        }
+        long long int ans=0,rem=0;
+        while(num != 0){
+            rem=num % 10;
+            ans=ans*10+rem;
+            num /= 10;
+        }
+        if(ans < INT_MIN || ans>INT_MAX){
             return 0;
-         }
-        rem=n%10;
-        ans=ans*10+rem;
-        n=n/10;
+        }
+        return isNegetive ? -ans : ans;
     }
-    return isnegetive?-ans:ans;
-}
 };
