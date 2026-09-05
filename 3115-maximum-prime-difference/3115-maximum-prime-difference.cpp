ae1633451dda236c5a2de146bc51fdfff51ba713
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-05 11:39:20 +0530

    Time: 82 ms (81.05%), Space: 108.6 MB (97.18%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     bool isprime(int n){
-        if(n==1){
-            return false;
-        }
+        if(n<2) return false;
         for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                return false;
-            }
+            if(n%i==0) return false;
         }
         return true;
     }
     int maximumPrimeDifference(vector<int>& nums) {
-        vector<int>arr;
+        int firstIndex;
+        int lastIndex;
         for(int i=0;i<nums.size();i++){
             if(isprime(nums[i])){
-                arr.push_back(i);
+                firstIndex=i;
+                break;
             }
         }
-        return abs(arr[arr.size()-1]-arr[0]);
+        for(int i=(nums.size()-1);i>=0;i--){
+            if(isprime(nums[i])){
+                lastIndex=i;
+                break;
+            }
+        }
+        return abs(lastIndex-firstIndex);
     }
 };
