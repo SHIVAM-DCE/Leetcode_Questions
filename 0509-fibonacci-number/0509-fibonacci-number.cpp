0193948c57b31cf4052124e56ed4891dac69ef59
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-06 23:02:47 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -7,6 +7,13 @@ public:
         if(n==1){
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+        return b;
     }
 };
