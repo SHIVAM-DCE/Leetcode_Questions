014b10ec477a7d3406022333068f474770f1fe48
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-07 10:09:01 +0530

    Time: 0 ms (100%), Space: 22.7 MB (18.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,21 +2,35 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int size=nums.size();
-        vector<int>ans;
-        int i=0;
-        int j=1;
-        int count=0;
-        ans.push_back(nums[0]);
-        for(int i=1;i<size;i++){
-            if(nums[i]!=ans[count]){
-                ans.push_back(nums[i]);
-                count+=1;
-            }
+    //     vector<int>ans;
+    //     int i=0;
+    //     int j=1;
+    //     int count=0;
+    //     ans.push_back(nums[0]);
+    //     for(int i=1;i<size;i++){
+    //         if(nums[i]!=ans[count]){
+    //             ans.push_back(nums[i]);
+    //             count+=1;
+    //         }
+    //     }
+    //     int res=ans.size();
+    //     for(int j=0;j<res;j++){
+    //         nums[j]=ans[j];
+    //     }
+    //     return res;
+
+
+    int i=1;
+    int j=1;
+    while(i<size){
+        if(nums[i]==nums[i-1]){
+            i+=1;
+        }else{
+            nums[j]=nums[i];
+            i++;
+            j++;
         }
-        int res=ans.size();
-        for(int j=0;j<res;j++){
-            nums[j]=ans[j];
-        }
-        return res;
     }
+    return j;
+     }
 };
