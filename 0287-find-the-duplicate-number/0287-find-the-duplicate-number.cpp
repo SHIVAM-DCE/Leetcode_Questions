0c29db7c9aebf8b679eb87286156b1db0d2426ac
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-09 20:13:30 +0530

    Time: 0 ms (100%), Space: 64.9 MB (99.52%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -14,26 +14,20 @@ public:
     //indexing method
     //negetive marking method(array modify nhi karna hai)
     //yaha par ho raha hai isliye isko use nhi karenge
-    // int ans=-1;
-    // for(int i=0;i<nums.size();i++){
-    //     int index=abs(nums[i]);
-
-    //     //already visited
-    //     if(nums[index]<0){
-    //         ans=index;
-    //         break;
-    //     }
-
-    //     //visited mark
-    //     nums[index] *=-1;
-    // }
-    // return ans;
+    for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]);
+            if (nums[index] < 0) {
+                return index;
+            }
+            nums[index] = -nums[index];
+        }
+        return -1;
 
 
     //positioning method
-    while(nums[0]!=nums[nums[0]]){
-        swap(nums[0],nums[nums[0]]);
-    }
-    return nums[0];
-    }
+    // while(nums[0]!=nums[nums[0]]){
+    //     swap(nums[0],nums[nums[0]]);
+    // }
+    // return nums[0];
+     }
 };
