2561ec08e08e6b8123aa6a39c306a63c35e9d40d
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-16 00:13:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (72.13%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -5,12 +5,13 @@ public:
 
         string hex_Map="0123456789abcdef";
         string ans="";
+        // Convert to unsigned int to handle negative numbers automatically
         unsigned int n=num;
 
         while(n!=0){
-            int rem = n&15;
+            int rem = n%16;
             ans+=hex_Map[rem];
-            n>>=4;
+            n/=16;
         }
         reverse(ans.begin(),ans.end());
         return ans;
