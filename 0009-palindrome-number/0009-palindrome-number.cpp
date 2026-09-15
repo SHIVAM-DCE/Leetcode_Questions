850a6c022223e71b00148cf7ca27889607b55ca7
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-15 18:01:01 +0530

    Time: 4 ms (30.48%), Space: 8.6 MB (64.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int n=x;
+        long long newNum=0;
+        while(n!=0){
+            int rem=n%10;
+            newNum=newNum*10+rem;
+            n=n/10;
+        }
+        if(x==newNum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
