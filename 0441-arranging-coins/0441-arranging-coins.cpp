e7788d5275b0b6db6e24a441b2e9346774e654dc
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-15 22:53:01 +0530

    Time: 0 ms (100%), Space: 8.7 MB (96.3%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            n-=i;
-            ans+=1;
+        // int ans=0;
+        // for(int i=1;i<=n;i++){
+        //     n-=i;
+        //     ans+=1;
+        // }
+        // return ans;
+
+        int s=1;
+        int e=n;
+        int ans;
+        while(s<=e){
+            long mid=s+(e-s)/2;
+            long coinNeed=mid*(mid+1)/2;
+            if(coinNeed<=n){
+                ans=mid;
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
         }
         return ans;
     }
