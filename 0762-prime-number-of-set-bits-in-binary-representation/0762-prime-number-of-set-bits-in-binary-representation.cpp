8d1e5808bddedff34ba7b5cdc4c7a957e9231945
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-16 00:53:53 +0530

    Time: 4 ms (45.02%), Space: 8.3 MB (15.26%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
+        //
         unordered_set<int>prime = {2,3,5,7,11,13,17,19};
         int count=0;
         for(int i=left;i<=right;i++){
             // __builtin_popcount(): to count number of one comes in it's binary form or setbits.
             // count() ek fxn hai jo batata hai ki value set me present hai ya nhi in form of 0 and 1.
-            if(prime.count(__builtin_popcount(i))){
-                count++;
-            }
+            if(prime.count(__builtin_popcount(i))) count++;
         }
         return count;
     }
