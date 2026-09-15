4acea7e179ce6eeb70ad96d0826510ca1a57cfe3
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-15 19:09:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.82%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -3,10 +3,10 @@ public:
     int titleToNumber(string columnTitle) {
         int ans=0;
         for(char c : columnTitle){
-            int value = c-'A'+1;
-            ans=ans*26+value;
+            // int value = c-'A'+1;
+            ans=ans*26+(c-'A'+1);
         }
         return ans;
-        
+
     }
 };
