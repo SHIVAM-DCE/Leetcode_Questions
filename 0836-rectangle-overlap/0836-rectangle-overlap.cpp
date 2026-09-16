72f8b05d1f7a377e7b9e2782096e16cddbd14ccf
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-17 00:47:00 +0530

    Time: 0 ms (100%), Space: 9.8 MB (79.97%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        // ye socho ki overlap kab nhi kar skta ha
-        // jab edge left me ho
-        bool isleft = rec1[0]>=rec2[2];
-        //jab edge right me ho
-        bool isright = rec1[2]<=rec2[0];
-        // jab edge upar me ho
-        bool isupper = rec1[3]<=rec2[1];
-        //jab edge niche ho
-        bool islower = rec1[1]>=rec2[3];
+        // // ye socho ki overlap kab nhi kar skta ha
+        // // jab edge left me ho
+        // bool isleft = rec1[0]>=rec2[2];
+        // //jab edge right me ho
+        // bool isright = rec1[2]<=rec2[0];
+        // // jab edge upar me ho
+        // bool isupper = rec1[3]<=rec2[1];
+        // //jab edge niche ho
+        // bool islower = rec1[1]>=rec2[3];
 
-        return !(isleft||isright||isupper||islower);
+        // return !(isleft||isright||isupper||islower);
+
+
+        return (rec1[0] < rec2[2] && rec2[0] < rec1[2]) && 
+               (rec1[1] < rec2[3] && rec2[1] < rec1[3]);
     }
 };
