629f11d1e6431a706bce1d4dd8b9375366b46b95
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-17 22:43:52 +0530

    Time: 5 ms (64.91%), Space: 17 MB (66.49%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+
+    int binarysearch(vector<int>&nums,int start,int value/*a[i]+k*/){
+        int end=nums.size()-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(nums[mid]==value){
+                return mid;
+            }
+            else if(value>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    int findPairs(vector<int>& nums, int k) {
+
+        //brut force method
+        // set<pair<int ,int>>ans;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(abs(nums[i]-nums[j])==k){
+        //             ans.insert({nums[i], nums[j]});
+        //         }
+        //     }
+        // }
+        // return ans.size();
+
+
+
+        //two pointer method
+        sort(nums.begin(),nums.end());
+        int size=nums.size();
+        set<pair<int,int>> ans;
+        int i=0,j=1;
+        while(j<size){
+            int diff=nums[j]-nums[i];
+            if(diff==k){
+                ans.insert({nums[i],nums[j]});
+                i++;
+                j++;
+            }else if(diff>k){
+                i++;
+            }else{
+                j++;
+            }
+            if(i==j){
+                j++;
+            }
+        }
+        return ans.size();
+
+
+
+
+
+
+        //binary search method:
+        // sort(nums.begin(),nums.end());
+        // set<pair<int,int>>ans;
+        // for(int i=0;i<nums.size();i++){
+        //     if(binarysearch(nums,i+1,nums[i]+k)!=-1){
+        //         ans.insert({nums[i],nums[i]+k});
+        //     }
+        // }
+        // return ans.size();
+    }
+};
