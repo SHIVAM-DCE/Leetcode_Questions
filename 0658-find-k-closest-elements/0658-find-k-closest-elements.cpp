50fa44d9c5f48ec588f26a989ce444c7557050d9
SHIVAM KUMAR <shivamkumarkaimur@gmail.com>
2026-09-19 23:42:06 +0530

    Time: 0 ms (100%), Space: 35.9 MB (67.93%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int lowerBound(vector<int>& arr, int x) {
+        int s = 0;
+        int e = arr.size() - 1;
+        int ans = arr.size(); 
+        
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> binarySearchmethod(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int h = lowerBound(arr, x);
+        int l = h - 1;
+        
+        // Pointers window ke bahar na jayein aur size exactly k maintain rahe
+        while (k > 0) {
+            if (l < 0) {
+                h++; // Left boundary khatam, right element extend karo
+            } else if (h >= n) {
+                l--; // Right boundary khatam, left element extend karo
+            } else if (x - arr[l] <= arr[h] - x) {
+                l--; // Left wala element paas hai ya tie hai (a < b rule)
+            } else {
+                h++; // Right wala element paas hai
+            }
+            k--;
+        }
+        
+        // Loop khatam hone par hamari safe aur correct window (l + 1) se lekar (h - 1) tak hai
+        return vector<int>(arr.begin() + l + 1, arr.begin() + h);
+    }
+
+
+
+
+    vector<int> twoPointermethod(vector<int>& arr, int k, int x) {
         int l=0;
         int h=arr.size()-1;
         while(h-k>=l){
@@ -11,5 +54,13 @@ public:
             }
         }
         return vector<int>(arr.begin()+l,arr.begin()+l+k);
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        //method 1
+        // return twoPointermethod(arr,k,x);
+
+
+        //method 2
+        return binarySearchmethod(arr,k,x);
     }
 };
